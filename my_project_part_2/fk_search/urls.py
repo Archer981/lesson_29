7ff7c8b477d9f375ecb8a03b1dd5d3ef89830361
buzov <abuzov@list.ru>
2023-06-01 10:34:46 +0300

    Homework completed

--- my_project_part_2/fk_search/urls.py
+++ my_project_part_2/fk_search/urls.py
@@ -1,5 +1,12 @@
 # TODO здесь необходимо настроить urls приложения
+from rest_framework import routers
+
+from fk_search.views import StoreView
+
+router = routers.SimpleRouter()
+router.register('fk_search', StoreView)
 
 urlpatterns = [
 
 ]
+urlpatterns += router.urls
--- my_project_part_2/qf_queries/urls.py
+++ my_project_part_2/qf_queries/urls.py
@@ -1,5 +1,12 @@
 # TODO здесь необходимо настроить urls приложения
+from rest_framework import routers
+
+from qf_queries.views import StoreView
+
+router = routers.SimpleRouter()
+router.register('qf_queries', StoreView)
 
 urlpatterns = [
 
 ]
+urlpatterns += router.urls
